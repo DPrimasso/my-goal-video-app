c3a86eee47686dd6f7e6da4724496ee4e303e778
Daniele Primasso <62555648+DPrimasso@users.noreply.github.com>
2025-08-02 21:37:49 +0200

    Prepopulate formation with default players

--- src/remotion/index.tsx
+++ src/remotion/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Composition, registerRoot} from 'remotion';
 import {MyGoalVideo, MyGoalVideoProps} from './MyGoalVideo';
 import {FormationVideo, FormationVideoProps} from './FormationVideo';
+import {players} from '../players';
 
 const RemotionRoot: React.FC = () => {
   const defaultProps: MyGoalVideoProps = {
@@ -11,12 +12,21 @@ const RemotionRoot: React.FC = () => {
     overlayImage: 'logo192.png',
   };
 
-    const formationDefaults: FormationVideoProps = {
-        goalkeeper: { name: 'Portiere', image: 'players/davide_fava.png' },
-        defenders: [],
-        midfielders: [],
-        forwards: [],
-    };
+  const mapFormationPlayer = (p: {name: string; image: string}) => ({
+    name: p.name,
+    image: p.image,
+  });
+
+  const formationDefaults: FormationVideoProps = {
+    goalkeeper: mapFormationPlayer(players[0] || {name: '', image: ''}),
+    defenders: [players[1], players[2], players[0], players[1]]
+      .filter(Boolean)
+      .map(mapFormationPlayer),
+    midfielders: [players[2], players[0], players[1], players[2]]
+      .filter(Boolean)
+      .map(mapFormationPlayer),
+    forwards: [players[0], players[1]].filter(Boolean).map(mapFormationPlayer),
+  };
 
   return (
     <>
