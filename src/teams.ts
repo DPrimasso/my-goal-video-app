483aacc33924f55ea19485c0a899e16bbf70cdb3
dprimo17 <daniele@secarepay.com>
2025-08-03 14:37:04 +0200

    wip

--- src/teams.ts
+++ src/teams.ts
@@ -7,9 +7,9 @@ export interface Team {
 const ASSET_BASE = process.env.REACT_APP_ASSET_BASE;
 
 export const teams: Team[] = [
-  { id: 'casalpoglio', name: 'Casalpoglio', logo: `${ASSET_BASE}/logo_casalpoglio.png` },
-  { id: 'amatori_club', name: 'Amatori Club', logo: `${ASSET_BASE}/logo_amatori_club.png` },
-  { id: 'team2', name: 'Team 2', logo: `${ASSET_BASE}/logo192.png` },
+  { id: 'casalpoglio', name: 'Casalpoglio', logo: `${ASSET_BASE}/logos/logo_casalpoglio.png` },
+  { id: 'amatori_club', name: 'Amatori Club', logo: `${ASSET_BASE}/logos/logo_amatori_club.png` },
+  { id: 'team2', name: 'Team 2', logo: `${ASSET_BASE}/logos/logo192.png` },
 ];
 
 export default teams;
